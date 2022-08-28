d4b713bf6c56878e365528b55b6209388f4cd8de
jsguerra <jose@gcostudios.com>
2022-08-28 21:53:31 +0100

    added providers

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,18 @@
 import '../styles/globals.css';
 
+import { MDXProvider } from '@mdx-js/react';
+
+import Layout from '../components/layouts/Layout';
+import MDXComponents from '../components/MDXComponents';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <MDXProvider components={MDXComponents}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </MDXProvider>
+  );
 }
 
 export default MyApp;
