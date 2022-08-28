b6967a2be4edbbb794617cbe1f0f65e82616bf1a
jsguerra <jose@gcostudios.com>
2022-08-28 14:59:32 +0100

    initial commit

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://siteAddress.com',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+};
+
+// export default config;
+module.exports = config;
